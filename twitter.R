d942aac130e21d4f7a98e3993b754d309e07054f
VK <hmirov@gmail.com>
2015-05-14 21:55:42 +0200

    Update twitter.R

--- twitter.R
+++ twitter.R
@@ -10,16 +10,16 @@ library(reshape2)
 # install this file:
 download.file(url="http://curl.haxx.se/ca/cacert.pem", destfile="cacert.pem")
 
-api_key <-"pZsNCkOSPb3oFlw6ZHf7q5rsc"
-api_secret <-"bIEimC1fRBNf5b9sQyL0LboFsj2Y5sUVkXvmfEOQXJtiQmllOU"
-access_token <-"75807850-pbK8CxU9DVN3T0XUbz0o56IdReNVqUs0ChpTIopU7"
-access_token_secret <-"KxUU8YAzsUirBKW9QAh2Df1dpSZvAUb3bte05hziDZdSh"
+api_key <-"11"
+api_secret <-"11"
+access_token <-"11"
+access_token_secret <-"11"
 
 requestURL <- "https://api.twitter.com/oauth/request_token"
 accessURL = "https://api.twitter.com/oauth/access_token"
 authURL = "http://api.twitter.com/oauth/authorize"
-consumerKey = "pZsNCkOSPb3oFlw6ZHf7q5rsc"
-consumerSecret = "bIEimC1fRBNf5b9sQyL0LboFsj2Y5sUVkXvmfEOQXJtiQmllOU"
+consumerKey = "11"
+consumerSecret = "11"
 
 
 Cred <- OAuthFactory$new(consumerKey=consumerKey,
